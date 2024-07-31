d94991edb24ed2622a5078a6110b48218ced54cb
Romana Faiglova <r.faiglova@gmail.com>
2024-07-31 10:49:38 +0200

    Fetch Products from API

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import {Footer, Header} from '../Components/Layout';
-
+import { useState, useEffect } from 'react';
 
 function App() {
+
+// use state with default array
+const [products, setProducts] = useState([]);
+
+// use effect to fetch data from api
+useEffect(() => {
+  fetch("https://ecommercewebappapi.azurewebsites.net/api/Product")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })  
+}, []);
+
+
   return (
     <div>
       <Header />
